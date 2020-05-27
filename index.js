454e3b05a76a9bc687bc92def3aa4e67280baa28
Amir Ali <amir.saeed.ali92@gmail.com>
2020-05-27 19:28:21 +0500

    latest build

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-import { of } from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { map, pluck, mergeMap, tap, takeWhile, delay } from 'rxjs/operators';
 
-const source$ = of(1, 2, 3);
-source$.subscribe(console.log);
+const button$ = fromEvent(document, 'keydown');
+
+const transformed$ = button$.pipe(
+    pluck("keyCode"),
+    map(code => {
+        const element = document.querySelector(`div[data-key='${code}']`);
+        const audio = document.querySelector(`audio[data-key='${code}']`);
+        element.classList.add('playing');
+        audio.currentTime = 0;
+        audio.play();
+        return element;
+    }),
+    delay(70),
+    tap(element => {
+        element.classList.remove('playing');
+    })
+)
+
+transformed$.subscribe();
+
